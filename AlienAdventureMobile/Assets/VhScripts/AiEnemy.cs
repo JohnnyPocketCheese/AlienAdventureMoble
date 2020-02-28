4c06a704a2d407199ab005105715ace560f424f9
Victor Hernandez <20.victor.hernandez@ksd.org>
2020-02-28 13:33:52 -0800

    added merge error

---
+++ AlienAdventureMobile/Assets/VhScripts/AiEnemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiEnemy : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+    public float agroRange;
+    public float moveSpeed;
+
+    Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //distance to player
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+        
+        if(distToPlayer < agroRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            StopChasingPlayer();
+        }
+    }
+    void ChasePlayer()
+    {
+        if(transform.position.x < player.position.x)
+        {
+            //enemy is to the left side of the player, so move right
+            rb2d.velocity = new Vector2(moveSpeed, 0);
+            transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            //enemy is to the right side of the player, so move left
+            rb2d.velocity = new Vector2(-moveSpeed, 0);
+            transform.localScale = new Vector2(-1, 1);
+        }
+    }
+
+    void StopChasingPlayer()
+    {
+        rb2d.velocity = Vector2.zero;
+    }
+}
