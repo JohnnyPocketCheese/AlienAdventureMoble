41b13641592c1e794e9fb94fbca2492a3a0ba1f9
Benjamin Rucker <21.benjamin.rucker@ksd.org>
2020-02-28 13:06:21 -0800

    making level two

---
+++ AlienAdventureMobile/Assets/Animations/levelfad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelfad : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetMouseButton(0))
+        {
+            fadetolevel(1);
+        }
+    }
+    public void fadetolevel (int levelIdex)
+    {
+        animator.SetTrigger("FADEOUT");
+    }
+}
